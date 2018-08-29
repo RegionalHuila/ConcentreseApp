c45f036d0d706913c658864e979c73dc8ee1d4c3
luis henry <lhreyes5@misena.edu.co>
2018-08-29 17:09:28 -0500

    base datos

--- app/src/main/java/com/example/worldskills/concentreseapp/SplashActivity.java
+++ app/src/main/java/com/example/worldskills/concentreseapp/SplashActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashActivity extends Activity {
 
@@ -12,6 +13,12 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        View decoracionVista = getWindow().getDecorView();
+
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decoracionVista.setSystemUiVisibility(uiOptions);
 
         new Handler().postDelayed(new Runnable() {
             @Override
