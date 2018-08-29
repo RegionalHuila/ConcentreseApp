af4d2aacd418ee2d0a79dec63803e29ba037ff6a
Brahiam Musse <bamusse@misena.edu.co>
2018-08-29 13:51:57 -0500

    1

---
+++ app/src/main/java/com/example/worldskills/concentreseapp/Juego_ST_Facil.java
@@ -0,0 +1,13 @@
+package com.example.worldskills.concentreseapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Juego_ST_Facil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juego__st__facil);
+    }
+}
