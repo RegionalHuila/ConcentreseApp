6f1d8a6056e1fddfc1e41e66576f481cc8ca66ef
luis henry <lhreyes5@misena.edu.co>
2018-08-29 19:03:18 -0500

    fondo

--- app/src/main/java/com/example/worldskills/concentreseapp/PuntajeActivity.java
+++ app/src/main/java/com/example/worldskills/concentreseapp/PuntajeActivity.java
@@ -1,7 +1,17 @@
 package com.example.worldskills.concentreseapp;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.worldskills.concentreseapp.BaseDatos.AyudaBaseDatos;
+import com.example.worldskills.concentreseapp.BaseDatos.TablaDatos;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PuntajeActivity extends AppCompatActivity {
 
@@ -9,5 +19,6 @@ public class PuntajeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puntaje);
+
     }
 }
